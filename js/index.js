caacf6bba64172908bd06e04d4b3006a5b8c7c3f
Pr0lamer <andrewmakmail@gmail.com>
2022-02-18 17:00:33 +0200

    copy to clipboard functin added + most of the styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/-+*#@!%^&()_[]{}';
+const generateBtn = document.querySelector("#generate-btn");
+const passwordBox = document.getElementsByClassName("password-box");
 
 function generator(length){
     let result = '';
@@ -10,4 +12,24 @@ function generator(length){
     return result;
 }
 
-console.log(generator(12));
+generateBtn.addEventListener("click", ()=>{
+    for(let i = 0; i<4; i++){
+        passwordBox[i].value = generator(12);
+    }
+
+});
+
+for(let i =0; i < passwordBox.length; i++){
+    passwordBox[i].addEventListener("click", ()=>{
+        /* Select the text field */
+        passwordBox[i].select();
+        passwordBox[i].setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(passwordBox[i].value);
+
+        /* Alert the copied text */
+        alert("Copied the text: " + passwordBox[i].value);
+    });
+}
+//console.log(generator(12));
